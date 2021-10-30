a2fb126cc164eb82e0a24f2c14ef6b86c8973a22
arnavgunjikar <81632011+arnavgunjikar@users.noreply.github.com>
2021-10-30 19:47:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,137 @@
+var timer_counter = 0;
+
+var Time = 0;
+
+var timer_check = 0;
+
+var answer_holder = 0;
+
+var drawn_sketch = 0;
+
+var sketch = 0;
+
+var score = 0;
+
+var quick_draw_data_set = 0;
+
+function preload() 
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function updateCanvas() {
+
+background("white");
+  
+random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+  
+console.log(quick_draw_data_set[random_number]);
+  
+sketch = quick_draw_data_set[random_number];
+  
+document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+  
+}
+  
+function setup()
+{
+  
+canvas = createCanvas(280, 280);
+  
+canvas.center();
+  
+background("white");
+
+canvas.mouseReleased(classifyCanvas);
+  
+}
+  
+function draw() {
+  
+check_sketch()
+  
+if(drawn_sketch == sketch)
+{
+  
+answer_holder = "set"
+  
+score++;
+  
+document.getElementById('score').innerHTML = 'Score: ' + score;
+  
+}
+
+// Set stroke weight to 10
+  
+strokeWeight(13);
+  
+// Set stroke color to black
+
+stroke(0);
+
+// If mouse is pressed, draw line between previous and current mouse positions
+
+if (mouseIsPressed) {
+
+  line(pmouseX, pmouseY, mouseX, mouseY);
+
+}
+
+} 
+  
+function check_sketch()
+  
+{
+  
+timer_counter++;
+  
+document.getElementById('timer').innerHTML = 'Timer: ' + timer_counter;
+  
+console.log(timer_counter)
+  
+if(timer_counter > 400)
+{
+
+timer_counter = 0;
+  
+timer_check = "completed"
+
+}
+
+if(timer_check =="completed" || answer_holder == "set")
+{
+  
+    timer_check = "";
+  
+    answer_holder = "";
+  
+    updateCanvas();
+  
+}
+
+}
+  
+function classifyCanvas() 
+{
+    classifier.classify(canvas, gotResult);
+}
+  
+  
+function gotResult(error, results) 
+{
+  
+if (error) 
+{
+    console.error(error);
+}
+  
+    console.log(results);
+  
+    drawn_sketch = results[0].label;
+  
+    document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch;
+  
+  
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+  
+} 
